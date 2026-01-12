4677b7e3b36d494bc87004016e8305e114fc504f
Mahmoud El-Boshy <mahmoud.esam.elboshy@gmail.com>
2026-01-12 16:18:26 +0200

    Forward ESP32-Camera frame to detection model

--- server/server.js
+++ server/server.js
@@ -20,29 +20,65 @@ const detectionNS = io.of('/detection');
 const devicesNS = io.of('/devices');
 const webInterfaceNS = io.of('/webinterface');
 
+const systemStatus = {
+  esp32camera_connected: false,
+  detectionModel_connected: false,
+  esp32_connected: false,
+  car_count: 0
+}
+
+const connection_ids = {
+  esp32camera_id: null,
+  detectionModel_id: null,
+  esp32_id: null
+}
+
 videoNS.on('connection', (socket) => {
   console.log('A new ESP32-Camera connected to the video namespace', 'socketID:', socket.id);
+  systemStatus.esp32camera_connected = true;
+  connection_ids.esp32camera_id = socket.id;
 
   socket.on('disconnect', () => {
     console.log('ESP32-Camera disconnected from the video namespace', 'socketID:', socket.id);
+    systemStatus.esp32camera_connected = false;
+    connection_ids.esp32camera_id = null;
+  });
+
+  socket.on('frame', (data) => {
+    // Relay the frame data to all connected clients in the detection namespace
+    detectionNS.volatile.emit('frame', data);
   });
 });
 
 
 detectionNS.on('connection', (socket) => {
   console.log('A new Detection Model connected to the detection namespace', 'socketID:', socket.id);
-  
+  systemStatus.detectionModel_connected = true;
+  connection_ids.detectionModel_id = socket.id;
+
   socket.on('disconnect', () => {
     console.log('Detection Model disconnected from the detection namespace', 'socketID:', socket.id);
+    systemStatus.detectionModel_connected = false;
+    connection_ids.detectionModel_id = null;
+  });
+
+  socket.on('detection_result', (data) => {
+    // Update car count and log it
+    systemStatus.car_count = data.car_count;
+    console.log('Updated Car Count:', systemStatus.car_count);
   });
 });
 
 
 devicesNS.on('connection', (socket) => {
   console.log('A new ESP32 connected to the devices namespace', 'socketID:', socket.id);
-  
+  systemStatus.esp32_connected = true;
+  connection_ids.esp32_id = socket.id;
+
   socket.on('disconnect', () => {
     console.log('ESP32 disconnected from the devices namespace', 'socketID:', socket.id);
+    systemStatus.esp32_connected = false;
+    connection_ids.esp32_id = null;
   });
 });
 
